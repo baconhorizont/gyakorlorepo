39c17950c345f9aad4dc3f9c0acb96e745eec765
baconhorizont <bboard16@gmail.com>
2022-07-28 09:23:05 +0200

    gyakorlo commit

--- src/main/java/gyakorlo.java
+++ src/main/java/gyakorlo.java
@@ -1,6 +1,8 @@
 public class gyakorlo {
     public static void main(String[] args) {
         System.out.println("gyakorlo");
+        System.out.println("gyakorlo");
+        System.out.println("gyakorlo");
     }
 
 }
