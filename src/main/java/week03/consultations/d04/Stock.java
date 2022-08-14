1d9a2c1df5f7e32d7f8f2233bcd68a0f23ac5a6b
baconhorizont <bboard16@gmail.com>
2022-08-14 21:59:30 +0200

    ex-week03

--- src/main/java/week03/consultations/d04/Stock.java
+++ src/main/java/week03/consultations/d04/Stock.java
@@ -5,22 +5,22 @@ import java.util.List;
 
 public class Stock {
 
-    private List<Double> numbers = new ArrayList<>();
+    private List<Double> numbers ;
 
     public Stock(List<Double> numbers) {
+
         this.numbers = numbers;
     }
 
     public double maxProfit(){
         double stockProfit = 0;
-        for (int i =1;i < numbers.size();i++){
-            if (numbers.get(i-1) < numbers.get(i)){
-                stockProfit = numbers.get(i);
+        for (int i = 0; i < numbers.size(); i++) {
+            for (int j = i + 1; j < numbers.size(); j++ )
+            if (numbers.get(j) - numbers.get(i) > stockProfit){
+                stockProfit = numbers.get(j) - numbers.get(i);
             }
         }
-
         return stockProfit;
-
     }
 
 }
